4ce6225a9c65781eb282427afdf76474f794b515
sorinradneantu <62335059+sorinradneantu@users.noreply.github.com>
2021-11-18 18:39:10 +0200

    TODO1&2 - registerLocationListener is done

--- app/src/main/java/ro/upt/sma/context/location/LocationHandler.kt
+++ app/src/main/java/ro/upt/sma/context/location/LocationHandler.kt
@@ -2,8 +2,10 @@ package ro.upt.sma.context.location
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Looper
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 
 @SuppressLint("MissingPermission")
@@ -12,9 +14,21 @@ class LocationHandler(context: Context) {
     private val client: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
 
     fun registerLocationListener(locationCallback: LocationCallback) {
-        // TODO 1: Create a LocationRequest with PRIORITY_HIGH_ACCURACY and smallest displacement to 10m.
 
-        // TODO 2: Register request and callback with the fused location service client.
+        //Create a LocationRequest with PRIORITY_HIGH_ACCURACY and smallest displacement to 10m.
+        val newLocationRequest = LocationRequest.create()?.apply {
+
+            smallestDisplacement = 10.toFloat()
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        }
+        //Register request and callback with the fused location service client.
+
+        client.requestLocationUpdates(
+            newLocationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
 
     }
 
